a1a6568da008c992a16d4ece14785f96f1dff767
davidi <davidi28her@gmail.com>
2024-11-18 20:40:02 +0200

    third version

--- ex1.c
+++ ex1.c
@@ -58,13 +58,13 @@ int main() {
   
   // 3, 5, 7, 11
   printf("\n3, 5, 7, 11:\n");
-  int number1, number2, sum;
+  long int number1, number2, sum;
   printf("Please enter an octal number of your choice:");
-  scanf("%o",&number1);
+  scanf("%lo",&number1);
   printf("Please enter another octal number of your choise:");
-  scanf("%o",&number2);
+  scanf("%lo",&number2);
   sum = number1 + number2;
-  printf("The sum in hex is:%x\n", sum);
+  printf("The sum in hex is:%lx\n", sum);
   printf("the 3,5,7,11 bits are:%d%d%d%d\n", (sum >> 3) & 1, (sum >> 5) & 1, (sum >> 7) & 1, (sum >> 11) & 1);
   /* Scan two integers in octal base
   sum them up and print the result in hexadecimal base
